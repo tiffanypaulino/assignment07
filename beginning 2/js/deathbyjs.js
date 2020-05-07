fe672507874dc8eaf5191f0f1dff75eee1595436
tiffanypaulino <56327590+tiffanypaulino@users.noreply.github.com>
2020-05-06 18:54:34 -0700

    Update deathbyjs.js

--- beginning 2/js/deathbyjs.js
+++ beginning 2/js/deathbyjs.js
@@ -1,11 +1,57 @@
 /*eslint-env browser*/
 
 //STEP 1
+function alphabet_order(str) {
+    return str.split('').sort().join('');
+}
+console.log(alphabet_order("littlebrother"));
 
 //STEP 2
+capitalize_Words 
+function capitalize_Words(str)
+{
+ return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+console.log(capitalize_Words('pure skin lounge'));
 
 //STEP 3
+function vowel_count(str1)
+{
+  var vowel_list = 'aeiouAEIOU';
+  var vcount = 0;
+  
+  for(var x = 0; x < str1.length ; x++)
+  {
+    if (vowel_list.indexOf(str1[x]) !== -1)
+    {
+      vcount += 1;
+    }
+  
+  }
+  return vcount;
+}
+console.log(vowel_count("Kuumomimakamae"));
 
 //STEP 4
+function makeid(l)
+{
+var text = "";
+var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+for(var i=0; i < l; i++ )
+{  
+text += char_list.charAt(Math.floor(Math.random() * char_list.length));
+}
+return text;
+}
+console.log(makeid(8));
 
 //STEP 5
+function Longest_Country_Name(country_name)
+  {
+  return country_name.reduce(function(lname, country) 
+  {
+    return lname.length > country.length ? lname : country;
+  }, 
+"");
+}
+console.log(Longest_Country_Name(["Argentina", "Dominican Republic", "United States of America"]));
